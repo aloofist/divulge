46f7e232bbfadcbbe9e91343fe280e3bf47cb060
aloofist <98794169+aloofist@users.noreply.github.com>
2024-04-15 21:34:51 +0800

    comment section

--- divulge/src/Pages/Post.jsx
+++ divulge/src/Pages/Post.jsx
@@ -50,40 +50,84 @@ const Post = () => {
 
   return (
     <>
-        <animated.div
-        id="circle"
-        style={{
-          position: 'absolute',
-          ...animatedCircle,
-        }}
-      ></animated.div>
-
-      <animated.div
-        id="blob"
-        style={{
-          position: 'absolute',
-          ...animatedBlob,
-        }}
-      ></animated.div>
+        
+        
 
         <Navbar/>
         <div className='post-card'>
+              <animated.div
+              id="circle"
+              style={{
+                position: 'absolute',
+                ...animatedCircle,
+              }}
+            ></animated.div>
+
+            <animated.div
+              id="blob"
+              style={{
+                position: 'absolute',
+                ...animatedBlob,
+              }}
+            ></animated.div>
+
             <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="" data-zoomable />
 
             <div className='post-info'>
-                <h1>Title</h1>
+                <h1>Celestial Reverie</h1>
                 <p>
                     <i>by</i> 
                     <i id='username'><b>Username</b></i>
 
                     <button>
                         <i class="ri-discord-fill"></i>
-                        <b id='discord-username'>aloofist</b>
+                        <b id='discord-username'>discord</b>
                     </button>
+
+                    <i>• 20h ago</i>
+                    
                 </p>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolores optio mollitia quidem enim! Quaerat, magni voluptates tenetur aspernatur ab quisquam explicabo sint veritatis, iste ratione officia quia exercitationem cum.</h2>
+                <h2>In the heart of a forgotten forest, where moonbeams dance upon dew-kissed leaves, lies an ancient stone circle. Within its moss-covered arches, ethereal beings gather under the silver glow of a crescent moon. Their luminous wings unfurl, casting delicate shadows on the weathered stones.</h2>
                 
             </div>
+
+            
+            <span className="divider"></span>
+
+            <div className='comment-section'>
+              
+            <div className='commenter'>
+              {/* <p>@You</p> */}
+              <input type="text" placeholder='Write a comment...'/>
+            </div>
+
+              <span className="divider"></span>
+              <div className='comment-section1'>
+
+                <div className="comment">
+                  <h4>@ProArtist58</h4>
+                  <p>Wow, I'm commenting on a post! Insane art man!</p>
+                </div>
+                <div className="comment">
+                  <h4>@StardustDreamer</h4>
+                  <p>This piece transports me to a realm where moonlight dances with magic.</p>
+                </div>
+                <div className="comment">
+                  <h4>@DivulgerFan</h4>
+                  <p>The way the colors blend feels like stardust settling on my soul. </p>
+                </div>
+                <div className="comment">
+                  <h4>@Kreekcraft</h4>
+                  <p>I can almost hear the celestial lute and taste the aurora elixir. Enchanting! </p>
+                </div>
+                <div className="comment">
+                  <h4>@TimelessWanderer</h4>
+                  <p>Moonbeams stitched into dreams—this is where we belong.</p>
+                </div>
+
+              </div>
+            </div>
+            
             <span className="divider"></span>
         </div>
         <MainGallery/>
--- divulge/src/components/MainGallery.jsx
+++ divulge/src/components/MainGallery.jsx
@@ -26,8 +26,10 @@ const MainGallery = () => {
         {images.map((imageUrl, index) => (
             <div key={index} className='gallery-item'>
               <a href="#post">
+
                 <img src={imageUrl} alt={`Gallery item ${index}`} />
-              </a>
+              
+            </a>
           </div>
         ))}
 
@@ -38,56 +40,84 @@ const MainGallery = () => {
         </div>
 
         <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          <a href="#post">
+            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          </a>
         </div>
         <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
-        </div>
-
-        <div className='gallery-item'>
-          <img src="./divulge_logo.jpg" alt="" />
-        </div>
-
-        <div className='gallery-item'>
-          <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          <a href="#post">
+            <img src="./divulge_logo.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
-        </div>
-        <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
-        </div>
-
-        <div className='gallery-item'>
-          <img src="./divulge_logo.jpg" alt="" />
+          <a href="#post">
+            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          <a href="#post">
+            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          </a>
+        </div>
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
-        </div>
-        <div className='gallery-item'>
-          <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
+          <a href="#post">
+            <img src="./divulge_logo.jpg" alt="" />
+          </a>
         </div>
 
         <div className='gallery-item'>
-          <img src="./divulge_logo.jpg" alt="" />
+          <a href="#post">
+            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+          </a>
+        </div>
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="./divulge_logo.jpg" alt="" />
+          </a>
         </div>
 
       </div>
