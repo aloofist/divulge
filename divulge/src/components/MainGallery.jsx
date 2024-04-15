b27d9699dab43778ea7221bb1716819694c6d152
aloofist <98794169+aloofist@users.noreply.github.com>
2024-04-16 06:16:10 +0800

    MainGallery.jsx

--- divulge/src/components/MainGallery.jsx
+++ divulge/src/components/MainGallery.jsx
@@ -47,18 +47,47 @@ const MainGallery = () => {
 
         <div className='gallery-item'>
           <a href="#post">
-            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
+            <img src="https://shorturl.at/alrHV" alt="" />
           </a>
         </div>
         <div className='gallery-item'>
           <a href="#post">
-            <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
+            <img src="https://storage.googleapis.com/pod_public/1300/150711.jpg" alt="" />
           </a>
         </div>
 
         <div className='gallery-item'>
           <a href="#post">
             <img src="./divulge_logo.jpg" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.pinimg.com/600x/e4/a6/bf/e4a6bf2730e133f8281faddcf681ca68.jpg" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.imgur.com/0AIh5nN.png" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://sparksgallery.com/wp-content/uploads/2023/03/mayur-deshpande-zZPeoLxLRyM-unsplash.jpg" alt="" />
+          </a>
+        </div>
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://shorturl.at/gwHLO" alt="" />
+          </a>
+        </div>
+
+        <div className='gallery-item'>
+          <a href="#post">
+            <img src="https://i.pinimg.com/564x/80/77/2b/80772b9c19d3da68d1130ec8d3f5e12a.jpg" alt="" />
           </a>
         </div>
 
@@ -87,36 +116,7 @@ const MainGallery = () => {
 
         <div className='gallery-item'>
           <a href="#post">
-            <img src="./divulge_logo.jpg" alt="" />
-          </a>
-        </div>
-
-        <div className='gallery-item'>
-          <a href="#post">
-            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
-          </a>
-        </div>
-
-        <div className='gallery-item'>
-          <a href="#post">
-            <img src="https://i.pinimg.com/564x/4f/32/8a/4f328a8851d8f8de2311261fae488c28.jpg" alt="" />
-          </a>
-        </div>
-
-        <div className='gallery-item'>
-          <a href="#post">
-            <img src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" />
-          </a>
-        </div>
-        <div className='gallery-item'>
-          <a href="#post">
-            <img src="https://i.pinimg.com/564x/02/ce/e9/02cee96c8bafe5407d9eca32d52f8ba8.jpg" alt="" />
-          </a>
-        </div>
-
-        <div className='gallery-item'>
-          <a href="#post">
-            <img src="./divulge_logo.jpg" alt="" />
+            <img src="https://i.pinimg.com/736x/34/72/d3/3472d3e9a462deb694d6861d5fda4f41.jpg" alt="" />
           </a>
         </div>
 
