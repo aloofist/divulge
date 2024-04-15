8716cea4b10dba698bf168e3094435bb1d73b822
aloofist <98794169+aloofist@users.noreply.github.com>
2024-04-16 07:56:13 +0800

    Update Footer.jsx

--- divulge/src/components/Footer.jsx
+++ divulge/src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         <h2>Divulge.</h2>
         <p>© 2024 - Aloofist -  <i><small>(my backend teammate disappeared)</small></i> | All rights reserved.</p>
         <div style={{display: 'flex', gap: '1rem'}}>
-          <a style={{color: "white"}} href="https://discord.com/invite/HHuDE58QYy" target="_"><small class="ri-discord-fill"></small></a>
-          <a style={{color: "white"}} href="https://twitter.com/aloofistprojs" target="_"><small class="ri-twitter-fill"></small></a>
-          <a style={{color: "white"}} href="https://github.com/aloofist" target="_blank"><small class="ri-github-fill" ></small></a>
+          <a style={{color: "white", transform: "scale(150%)"}} href="https://discord.com/invite/HHuDE58QYy" target="_"><small class="ri-discord-fill"></small></a>
+          <a style={{color: "white", transform: "scale(150%)"}} href="https://twitter.com/aloofistprojs" target="_"><small class="ri-twitter-fill"></small></a>
+          <a style={{color: "white", transform: "scale(150%)"}} href="https://github.com/aloofist" target="_blank"><small class="ri-github-fill" ></small></a>
         </div>
     </div>
   )
